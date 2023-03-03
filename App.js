8559489ed0df7791a0e9cbec51627c0e98ff9454
Miguel Aguiar <miguel_aguiar2016@outlook.com>
2023-03-03 18:22:06 -0300

    alteração de posição emoji

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
-
-import { Text, View, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
 
 import CardProduto from './components/CardProduto'
 
@@ -7,29 +10,44 @@ import Constants from 'expo-constants';
 
 export default function App() {
 
-    const filme1 = {
-    nome:"Beef Burger",
-    calorias : "🔥 70 calories",
+  const filme1 = {
+    nome: "Beef Burger",
+    emoji: "🔥",
+    calorias: "70 calories",
     preco: "$12.00",
-    poster: {uri: 'https://img.freepik.com/fotos-gratis/saboroso-hamburguer-de-carne-com-queijo-e-salada-de-frente-no-fundo-escuro_140725-89597.jpg'}
+    poster: {
+      uri: 'https://img.freepik.com/fotos-gratis/saboroso-hamburguer-de-carne-com-queijo-e-salada-de-frente-no-fundo-escuro_140725-89597.jpg'
+    }
   }
 
-    const filme2 = {
+  const filme2 = {
     nome: "Pancakes",
-    calorias : "🔥60 calories",
+    emoji: "🔥",
+    calorias: "60 calories",
     preco: "$15.00",
-    poster: {uri: 'https://moinhoglobo.com.br/wp-content/uploads/2015/10/panqueca-americana-1.jpg'}
+    poster: {
+      uri: 'https://moinhoglobo.com.br/wp-content/uploads/2015/10/panqueca-americana-1.jpg'
+    }
 
   }
 
 
 
-  return (
-    <View style={styles.container}>
-    <CardProduto filme={filme1}/>
-    <CardProduto filme={filme2}/>
-    </View>
-    
+  return ( <
+    View style = {
+      styles.container
+    } >
+    <
+    CardProduto filme = {
+      filme1
+    }
+    /> <
+    CardProduto filme = {
+      filme2
+    }
+    /> <
+    /View>
+
   );
 }
 
@@ -41,5 +59,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#eeeeee',
     padding: 8,
   },
-    
+
 });
